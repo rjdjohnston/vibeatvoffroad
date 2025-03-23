479006496b5b56c15ef1c94c912c1715595181c3
RJ Johnston <rj@broadcasting.inc>
2025-03-23 17:14:56 -0400

    feat: deploying with docker

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ io.on('connection', (socket) => {
 
 // Start server
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
